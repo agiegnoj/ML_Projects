b2da969bbd989a88d8b76f0d70bda4a50b99a857
Jon Geiger Gajate <jongg97@gmail.com>
2026-05-23 15:32:13 +0200

    .

---
+++ ddqn_&_ppo/PPO/Network.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+class Network(nn.Module):
+    """MLP for PPO"""
+    def __init__(self,  numObservations, numActions):
+        super().__init__()
+        self.layers = nn.Sequential(
+           nn.Linear(numObservations, 256),
+           nn.Tanh(),
+           nn.Dropout(0.1),
+           nn.Linear(256, 256),
+           nn.Tanh(),
+           nn.Dropout(0.1),
+           nn.Linear(256, 256),
+           nn.Tanh(),
+           nn.Dropout(0.1),
+           nn.Linear(256, numActions),
+           nn.Dropout(0.1)
+       )
+    def forward(self, x):
+        return torch.tanh(self.layers(x))

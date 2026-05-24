20dcd84aa6b8df42fbf9cec72cacac1eb00cd51b
Jon Geiger Gajate <jongg97@gmail.com>
2026-05-24 19:58:31 +0200

    .
    Removed dropout layers from QNetwork architecture.

--- ddqn_&_ppo/DDQN/QNetwork.py
+++ ddqn_&_ppo/DDQN/QNetwork.py
@@ -7,13 +7,8 @@ class QNetwork(nn.Module):
         self.layers = nn.Sequential(
             nn.Linear(observationDim, 256),
             nn.ReLU(),
-            nn.Dropout(0.1),
             nn.Linear(256, 256),
             nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(256, 256),
-            nn.ReLU(),
-            nn.Dropout(0.1),
             nn.Linear(256, actionDim),
         )
 
